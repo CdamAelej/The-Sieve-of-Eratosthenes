66078001f1013fa80584f024db3dbb917722d334
Adam Celej <celejadam01@gmail.com>
2022-01-11 12:37:35 +0100

    Add files via upload

---
+++ SitoErastotenesa.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Celej - sito Erastotenesa
+    cout << "Moje sito Erastotenesa by Adam Celej" << endl;
+    int n, w, i;
+    bool T[n];
+    cout << "Podaj zakres liczb: " << endl;
+    cout  >> n=100;
+    T[]=true;
+    i=2;
+    while (i<=n)
+        {
+            w=2*i;
+            T[w]=false;
+        }
+    return 0;
+}
